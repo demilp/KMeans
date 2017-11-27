598c65b35faa26db9347fac45b7cd2b9f9f9928d
Demián Levy Polat <demilp@gmail.com>
2017-11-27 02:04:26 -0300

    added test instances

--- __main__.py
+++ __main__.py
@@ -6,13 +6,22 @@ def run():
     points = np.vstack(((np.random.randn(150, 2) * 0.75 + np.array([2, 0])),
                         (np.random.randn(50, 2) * 0.25 + np.array([-1.5, 0.5])),
                         (np.random.randn(50, 2) * 0.5 + np.array([-0.5, -0.5]))))
-
+    K = 3
     kmeans = KMeans.KMeans()
-    kmeans.fit(points, 3, 10)
-    plt.scatter(points[:, 0], points[:, 1])
+    kmeans.fit(points, K, 10)
+    #plt.scatter(points[:, 0], points[:, 1])
     c = kmeans.centroids
-    plt.scatter(c[:, 0], c[:,1])
+
+    t = np.random.randn(1000, 2) * 1
+    cid = kmeans.predict(t)
+    for k in np.arange(K):
+        w = t[cid==k]
+        plt.scatter(w[:, 0], w[:, 1])
+
+    plt.scatter(c[:, 0], c[:, 1])
+
     plt.show()
+
 
 
 
